d3309b739ef2faa6459277a138984a4149ff04e8
AlexChen <alexchen@misfit.com>
2016-09-04 01:51:13 +0800

    Add recongization

--- polite_alex.py
+++ polite_alex.py
@@ -5,50 +5,69 @@
 # date: 2/9/2016
 # All copyright reserved to xsunset
 # ---------------------------------
-import time
-import subprocess
-
 from __future__ import absolute_import, division, print_function, \
     with_statement
 
+import time
+import subprocess
+from facepp import API, File
+import pygame
+
+
+API_KEY = '2d0df4c56a364d82eed3d1a238950ffb'
+API_SECRET = 'QovGDvkrsfOjY_ncbvT7ItprS6Af7gZZ'
+api = API(API_KEY, API_SECRET)
+
+
+
 class PoliteAlex(object):
     last_path = None
+    def __init__(self):
+        pygame.mixer.init()
+
 
     def capture_still_image(self):
+        return "imgs/test1.jpg"
         t = time.strftime("%Y%m%d_%H%M%S")
         path = "imgs/%s" % t
-        popen = subprocess.Popen(['raspstill','-o',path])
+        popen = subprocess.Popen(['raspstill','-w','200','-h','200','-o',path])
         ret = popen.communicate()
         return path
 
-    def is_screen_changed(self):
-        pass
+    def is_screen_changed(self, new_path, old_path):
+        return True
 
-    def is_somebody_in_picture(self):
-        pass
+    def is_somebody_in_picture(self, path):
+        return True
 
     def recognize_with_facepp(self, path):
 
-        rst = api.recognition.identify(group_name = 'test', url = TARGET_IMAGE)
+        # rst = api.recognition.identify(group_name = 'test', url = TARGET_IMAGE)
+        rst = api.recognition.identify(group_name = 'piface', img = File(path))
         print('recognition result', rst)
-        print '=' * 60
-        print 'The person with highest confidence:', \
-                rst['face'][0]['candidate'][0]['person_name']
+        print('=' * 60)
+        if len(rst['face']) > 0 and len(rst['face'][0]['candidate']) > 0:
+            print('The person with highest confidence:', \
+                rst['face'][0]['candidate'][0]['person_name'])
+        return rst
 
-    def is_master_in_picture(self):
-        pass
+    def is_master_in_picture(self, regn):
+        return True
 
-    def is_only_one_stranger_in_picture(self):
-        pass
+    def is_only_one_stranger_in_picture(self, regn):
+        return False
 
-    def is_master_with_stranger(self):
-        pass
+    def is_master_with_stranger(self, regn):
+        return False
 
     def welcom_master_with_stranger(self):
         pass
 
     def welcome_master(self):
-        pass
+        pygame.mixer.music.load("res/welcome_master.m4a")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
 
     def yield_at_stranger(self):
         pass
@@ -56,7 +75,7 @@ class PoliteAlex(object):
     def run_forever(self):
         while True:
             path = self.capture_still_image()
-            changed = self.is_screen_changed(path, last_path)
+            changed = self.is_screen_changed(path, self.last_path)
             if not changed:
                 # Sleep
                 continue
@@ -66,11 +85,11 @@ class PoliteAlex(object):
                 continue
 
             ret = self.recognize_with_facepp(path)
-            if self.is_master_with_stranger():
+            if self.is_master_with_stranger(ret):
                 self.welcom_master_with_stranger()
-            elif self.is_master_in_picture():
+            elif self.is_master_in_picture(ret):
                 self.welcome_master()
-            elif self.is_only_one_stranger_in_picture():
+            elif self.is_only_one_stranger_in_picture(ret):
                 self.yield_at_stranger()
 
 
